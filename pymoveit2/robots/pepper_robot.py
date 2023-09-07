ab62566a6290cdcdc91926771a06568499cd30af
HibaSekkat <hibasekkat136@gmail.com>
2023-09-07 12:11:24 +0100

    fix group_name

--- pymoveit2/robots/pepper_robot.py
+++ pymoveit2/robots/pepper_robot.py
@@ -1,25 +1,40 @@
 from typing import List
 
-MOVE_GROUP_ARM: str = "arm"
-MOVE_GROUP_GRIPPER: str = "gripper"
+MOVE_GROUP_ARM: str = "left_arm"
+MOVE_GROUP_GRIPPER: str = "LHand"
 
 OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [
-    0.2,
-    0.2,
-    0.2,
-    0.2,
-    0.2,
-    0.2,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
+    1.3,
 ]
 CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
-    1.3,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
+    0.2,
 ]
-
 
 def joint_names(prefix: str = "pepper_robot") -> List[str]:
     return [
@@ -32,20 +47,38 @@ def joint_names(prefix: str = "pepper_robot") -> List[str]:
     ]
 
 
-def base_link_name(prefix: str = "pepper_robot") -> str:
-    return prefix + "link_base"
-
-
-def end_effector_name(prefix: str = "pepper_robot") -> str:
-    return prefix + "end_effector"
-
-
-def gripper_joint_names(prefix: str = "pepper_robot") -> List[str]:
+def joint_names(prefix: str = "") -> List[str]:
     return [
-        prefix + "LFinger1",
-        prefix + "LFinger2",
-        prefix + "LFinger3",
-        prefix + "LFinger4",
-        prefix + "LThumb1",
-        prefix + "LThumb2",
+        "LShoulderPitch",
+        "LShoulderRoll",
+        "LElbowYaw",
+        "LElbowRoll",
+        "LWristYaw",
+        "LHand",
+    ]
+
+def base_link_name(prefix: str = "") -> str:
+    return "base_link"
+
+
+def end_effector_name(prefix: str = "") -> str:
+    return "left_eef"
+
+
+def gripper_joint_names(prefix: str = "") -> List[str]:
+    return [
+        "LFinger11",
+        "LFinger12",
+        "LFinger13",
+        "LFinger21",
+        "LFinger22",
+        "LFinger23",
+        "LFinger31",
+        "LFinger32",
+        "LFinger33",
+        "LFinger41",
+        "LFinger42",
+        "LFinger43",
+        "LThumb1",
+        "LThumb2",
     ]
